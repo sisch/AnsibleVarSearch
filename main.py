e7cdb3f5a43fd407d2c973ba7b6630a845df68bb
Simon Schliesky <simon@schliesky.com>
2018-03-27 16:13:56 +0200

    Implement markdwn output

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ searchpath = "./"
 variables = dict()
 
 
-def find_usage():
+def find_vars():
     var_usage_re = re.compile(r"{{\s*(\S+)\s*}}")
+    var_declaration_re = re.compile(r'"[^"]+"|(\w+):')
     for root, subdirs, files in walk(path.relpath(searchpath)):
         for file in files:
             # TODO: reduce files to templates/ and *.yml
@@ -25,16 +26,41 @@ def find_usage():
                         p = path.join(root, file)
                         if p not in variables[match]["usage"]:
                             variables[match]["usage"].append(p)
+                    for match in var_declaration_re.findall(line):
+                        variables.setdefault(match, {
+                            "usage": [],
+                            "declaration": []
+                        })
+                        p = path.join(root, file)
+                        if p not in variables[match]["declaration"]:
+                            variables[match]["declaration"].append(p)
+
     DEBUG(variables, level=2)
+
+
+def print_markdown():
+    for var in variables.keys():
+        if not variables[var]["usage"]:
+            continue
+        print("## ", var)
+        print("### Usage")
+        for u in variables[var]["usage"]:
+            print("* ", u)
+        print()
+        print("### Declaration")
+        for d in variables[var]["declaration"]:
+            print("* ", d)
+        print()
 
 
 def DEBUG(*args, level=1):
     if debug_level >= level:
-        print(args)
+        print(args, file=sys.stderr)
 
 
 def run():
-    find_usage()
+    find_vars()
+    print_markdown()
 
 
 if __name__ == "__main__":
